bcbc168eec06b98264e80edb93c3b98be3c04b63
Tavi <tavirazin@gmail.com>
2022-10-08 23:53:07 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,9 @@
 import React from "react";
-import About from "./About";
-import Awards from "./Awards";
-import Education from "./Education";
-import Experience from "./Experience";
-import Interests from "./Interests";
-import Skills from "./Skills";
-
 
 const Home = () => {
     return (
         <div>
-            <About />
-            <Awards />
-            <Education />
-            <Experience />
-            <Interests />
-            <Skills />
+            <h1>Welcome to my Portofolio</h1>
         </div>
     );
 };
