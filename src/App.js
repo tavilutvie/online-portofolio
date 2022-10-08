a9df4602b8cd7ac580b8dda551c23468ece0f41d
Tavi <tavirazin@gmail.com>
2022-10-09 00:13:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
       </div>
       <div class='split content'>
         <main>
-          <img id="bg" src="./images/foto4.jpg" alt="Background" />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path="/about" element={<About />} />
