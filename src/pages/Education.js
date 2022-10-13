d81508ad768535f343cd9d5ab20c934caf67929a
Tavi <tavirazin@gmail.com>
2022-10-13 18:51:40 +0700

    Update Education.js

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -136,10 +136,8 @@ const Education = () => {
                 <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
                   <Image
                     transform="scale(1.0)"
-                    src={
-                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                    }
-                    alt="some text"
+                    src={'images/logo-smala.png'}
+                    alt="smala"
                     objectFit="contain"
                     width="100%"
                     transition="0.3s ease-in-out"
@@ -149,28 +147,85 @@ const Education = () => {
                   />
                 </Link>
               </Box>
-              <BlogTags tags={['Engineering', 'Product']} marginTop="3" />
               <Heading fontSize="xl" marginTop="2">
                 <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                  Some blog title
+                  SMA Negeri 5 Surabaya
                 </Link>
               </Heading>
+              <BlogTags tags={['Surabaya', 'Jawa Timur', 'Indonesia']} marginTop="3" />
               <Text as="p" fontSize="md" marginTop="2">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
+                Major&emsp;: Science
+                <br></br>
+                Year&emsp;: 2017 - 2020
               </Text>
-              <BlogAuthor
-                name="John Doe"
-                date={new Date('2021-04-06T19:01:27Z')}
-              />
             </Box>
           </WrapItem>
         </Wrap>
-    
+        <Wrap spacing="30px" marginTop="5">
+          <WrapItem width={{ base: '100%', sm: '45%', md: '45%', lg: '30%' }}>
+            <Box w="100%">
+              <Box borderRadius="lg" overflow="hidden">
+                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                  <Image
+                    transform="scale(1.0)"
+                    src={'images/logo-smala.png'}
+                    alt="smala"
+                    objectFit="contain"
+                    width="100%"
+                    transition="0.3s ease-in-out"
+                    _hover={{
+                      transform: 'scale(1.05)',
+                    }}
+                  />
+                </Link>
+              </Box>
+              <Heading fontSize="xl" marginTop="2">
+                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                  SMA Negeri 5 Surabaya
+                </Link>
+              </Heading>
+              <BlogTags tags={['Surabaya', 'Jawa Timur', 'Indonesia']} marginTop="3" />
+              <Text as="p" fontSize="md" marginTop="2">
+                Major&emsp;: Science
+                <br></br>
+                Year&emsp;: 2017 - 2020
+              </Text>
+            </Box>
+          </WrapItem>
+        </Wrap>
+        <Wrap spacing="30px" marginTop="5">
+          <WrapItem width={{ base: '100%', sm: '45%', md: '45%', lg: '30%' }}>
+            <Box w="100%">
+              <Box borderRadius="lg" overflow="hidden">
+                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                  <Image
+                    transform="scale(1.0)"
+                    src={'images/logo-smala.png'}
+                    alt="smala"
+                    objectFit="contain"
+                    width="100%"
+                    transition="0.3s ease-in-out"
+                    _hover={{
+                      transform: 'scale(1.05)',
+                    }}
+                  />
+                </Link>
+              </Box>
+              <Heading fontSize="xl" marginTop="2">
+                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                  SMA Negeri 5 Surabaya
+                </Link>
+              </Heading>
+              <BlogTags tags={['Surabaya', 'Jawa Timur', 'Indonesia']} marginTop="3" />
+              <Text as="p" fontSize="md" marginTop="2">
+                Major&emsp;: Science
+                <br></br>
+                Year&emsp;: 2017 - 2020
+              </Text>
+            </Box>
+          </WrapItem>
+        </Wrap>
         </Flex>
-        
       </Container>
     );
   };
