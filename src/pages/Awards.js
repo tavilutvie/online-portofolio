debafca92cf80ceff718b8a5d6ab585f7e2f3c56
Tavi <tavirazin@gmail.com>
2022-10-17 13:34:09 +0700

    update

--- src/pages/Awards.js
+++ src/pages/Awards.js
@@ -1,17 +1,52 @@
-import './Awards.css';
-
-const Awards = () => {
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Button,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+    useColorModeValue,
+    VisuallyHidden,
+    List,
+    ListItem,
+} from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { MdLocalShipping } from 'react-icons/md';
+import Carousel3 from '../components/Carousel3';
+  
+  export default function Simple() {
     return (
-        <div>
-            <h1>Awards</h1>
-            <ul>
-                <li>
-                    <p>Contest winner of Manado Singing City 2018 International Choral Festival and Grandprix.</p>
-                    <img className="awards1"  src="images/foto3.jpg" alt="manado singing city championship" />
-                </li>
-            </ul>
-        </div>
-    )
-}
-
-export default Awards;
+      <Container maxW={'7xl'}>
+        <SimpleGrid
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 18, md: 24 }}>
+            <Stack spacing={{ base: 6, md: 10 }}>
+                <Box as={'header'}>
+                <Heading
+                    lineHeight={1.1}
+                    fontWeight={600}
+                    textAlign={'center'}
+                    fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+                    Category Champion of Manado Singing City 2018 International Choral Festival and Grandprix
+                </Heading>
+                <Text
+                    textAlign={'center'}
+                    color={useColorModeValue('gray.900', 'gray.400')}
+                    fontWeight={300}
+                    fontSize={'2xl'}>
+                    Manado, Sulawesi Utara, Indonesia
+                </Text>
+                </Box>
+                
+            </Stack>
+            <Carousel3 />
+        </SimpleGrid>
+      </Container>
+    );
+  }
